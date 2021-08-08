a933501d8e35b753725eb95f0de1164394df4f54
Caroline Tan <carolinectan3@gmail.com>
2021-08-08 14:34:48 -0400

    Add tests for API; revist mocking and stubbing for APIs for later

--- app/services/api_service.rb
+++ app/services/api_service.rb
@@ -1,7 +1,6 @@
 class ApiService
   def get_data(endpoint)
     response = Faraday.get(endpoint)
-    require "pry"; binding.pry
     data = response.body
     JSON.parse(data, symbolize_names: true)
   end
--- spec/features/bulk_discounts/index_spec.rb
+++ spec/features/bulk_discounts/index_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require '././app/poros/holidays.rb'
 
 RSpec.describe "merchant's bulk discounts index page" do
   before :each do
@@ -47,11 +48,87 @@ RSpec.describe "merchant's bulk discounts index page" do
     expect(current_path).to eq(merchant_bulk_discount_path(@merchant1.id, @bulk_discount1.id))
   end
 
-  it "has a section with a header of 'Upcoming Holidays'" do
-    expect(page).to have_content('Upcoming Holidays')
+  describe 'holidays API' do
+    # before :each do
+    # https://github.com/BrianZanti/intro_to_apis/tree/main/lib
+    #   mock_response =
+    #   '{
+    #     "date": "2021-11-11",
+    #     "localName": "Veterans Day",
+    #     "name": "Veterans Day",
+    #     "countryCode": "US",
+    #     "fixed": false,
+    #     "global": true,
+    #   },
+    #   {
+    #     "date": "2021-11-25",
+    #     "localName": "Thanksgiving Day",
+    #     "name": "Thanksgiving Day",
+    #     "countryCode": "US",
+    #     "fixed": false,
+    #     "global": true,
+    #     "launchYear": 1863,
+    #   },
+    #   {
+    #     "date": "2021-12-24",
+    #     "localName": "Christmas Day",
+    #     "name": "Christmas Day",
+    #     "countryCode": "US",
+    #     "fixed": false,
+    #     "global": true,
+    #   },
+    #   {
+    #     "date": "2021-12-31",
+    #     "localName": "New Year's Day",
+    #     "name": "New Year's Day",
+    #     "countryCode": "US",
+    #     "fixed": false,
+    #     "global": true
+    #   }'
+    #
+    #   allow_any_instance_of(Faraday::Response).to receive(:body).and_return(mock_response)
+    # end
 
-# In this section the name and date of the next 3 upcoming US holidays are listed.
-#
-# Use the Next Public Holidays Endpoint in the [Nager.Date API](https://date.nager.at/swagger/index.html)
+    it "has a section with a header of 'Upcoming Holidays'" do
+      expect(page).to have_content('Upcoming Holidays')
+    end
+
+    xit 'exists' do
+      holiday = Holiday.new(repo)
+
+      expect(holiday).to be_an_instance_of(Holiday)
+    end
+
+    xit 'receives the user infor and stores relevant attributes' do
+      holiday = Holiday.new(repo)
+
+      allow(holiday).to receive(:name1).and_return('Veterans Day')
+      expect(holiday.name1).to eq('Veterans Day')
+
+      allow(holiday).to receive(:date1).and_return('2021-11-11')
+      expect(holiday.date1).to eq('2021-11-11')
+
+      allow(holiday).to receive(:name2).and_return('Thanksgiving Day')
+      expect(holiday.name2).to eq('Thanksgiving Day')
+
+      allow(holiday).to receive(:date2).and_return('2021-11-25')
+      expect(holiday.date2).to eq('2021-11-25')
+
+      allow(holiday).to receive(:name3).and_return('Christmas Day')
+      expect(holiday.name3).to eq('Christmas Day')
+
+      allow(holiday).to receive(:date3).and_return('2021-12-24')
+      expect(holiday.date3).to eq('2021-12-24')
+    end
+
+    # Use the Next Public Holidays Endpoint in the [Nager.Date API](https://date.nager.at/swagger/index.html)
+    it 'displays the name and date of the next 3 upcoming US holidays' do
+      expect(page).to have_content('Labour Day')
+      expect(page).to have_content('2021-09-06')
+      expect(page).to have_content('Columbus Day')
+      expect(page).to have_content('2021-10-11')
+      expect(page).to have_content('Veterans Day')
+      expect(page).to have_content('2021-11-11')
+    end
   end
 end
