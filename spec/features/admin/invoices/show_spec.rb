9f5b311d4d113f7c63c36ed4584e2b3116b87dd9
Caroline Tan <carolinectan3@gmail.com>
2021-08-10 16:38:13 -0400

    Add admin total revenue

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -1,72 +1,77 @@
 require 'rails_helper'
 
-describe 'Admin Invoices Index Page' do
+describe 'Admin Invoices Show Page' do
   before :each do
-    @m1 = Merchant.create!(name: 'Merchant 1')
+    @merchant1 = Merchant.create!(name: 'Merchant 1')
 
-    @c1 = Customer.create!(first_name: 'Yo', last_name: 'Yoz', address: '123 Heyyo', city: 'Whoville', state: 'CO', zip: 12345)
-    @c2 = Customer.create!(first_name: 'Hey', last_name: 'Heyz')
+    @customer1 = Customer.create!(first_name: 'Yo', last_name: 'Yoz', address: '123 Heyyo', city: 'Whoville', state: 'CO', zip: 12345)
+    @customer2 = Customer.create!(first_name: 'Hey', last_name: 'Heyz')
 
-    @i1 = Invoice.create!(customer_id: @c1.id, status: 2, created_at: '2012-03-25 09:54:09')
-    @i2 = Invoice.create!(customer_id: @c2.id, status: 1, created_at: '2012-03-25 09:30:09')
+    @invoice1 = Invoice.create!(customer_id: @customer1.id, status: 2, created_at: '2012-03-25 09:54:09')
+    @invoice2 = Invoice.create!(customer_id: @customer2.id, status: 1, created_at: '2012-03-25 09:30:09')
 
-    @item_1 = Item.create!(name: 'test', description: 'lalala', unit_price: 6, merchant_id: @m1.id)
-    @item_2 = Item.create!(name: 'rest', description: 'dont test me', unit_price: 12, merchant_id: @m1.id)
+    @item1 = Item.create!(name: 'test', description: 'lalala', unit_price: 6, merchant_id: @merchant1.id)
+    @item2 = Item.create!(name: 'rest', description: 'dont test me', unit_price: 12, merchant_id: @merchant1.id)
 
-    @ii_1 = InvoiceItem.create!(invoice_id: @i1.id, item_id: @item_1.id, quantity: 12, unit_price: 2, status: 0)
-    @ii_2 = InvoiceItem.create!(invoice_id: @i1.id, item_id: @item_2.id, quantity: 6, unit_price: 1, status: 1)
-    @ii_3 = InvoiceItem.create!(invoice_id: @i2.id, item_id: @item_2.id, quantity: 88, unit_price: 12, status: 2)
+    @ii1 = InvoiceItem.create!(invoice_id: @invoice1.id, item_id: @item1.id, quantity: 12, unit_price: 20, status: 0)
+    @ii2 = InvoiceItem.create!(invoice_id: @invoice1.id, item_id: @item2.id, quantity: 6, unit_price: 10, status: 1)
+    @ii3 = InvoiceItem.create!(invoice_id: @invoice2.id, item_id: @item2.id, quantity: 88, unit_price: 1200, status: 2)
 
-    visit admin_invoice_path(@i1)
+    visit admin_invoice_path(@invoice1)
   end
 
   it 'should display the id, status and created_at' do
-    expect(page).to have_content("Invoice ##{@i1.id}")
-    expect(page).to have_content("Created on: #{@i1.created_at.strftime("%A, %B %d, %Y")}")
+    expect(page).to have_content("Invoice ##{@invoice1.id}")
+    expect(page).to have_content("Created on: #{@invoice1.created_at.strftime("%A, %B %d, %Y")}")
 
-    expect(page).to_not have_content("Invoice ##{@i2.id}")
+    expect(page).to have_no_content("Invoice ##{@invoice2.id}")
   end
 
   it 'should display the customers name and shipping address' do
-    expect(page).to have_content("#{@c1.first_name} #{@c1.last_name}")
-    expect(page).to have_content(@c1.address)
-    expect(page).to have_content("#{@c1.city}, #{@c1.state} #{@c1.zip}")
+    expect(page).to have_content("#{@customer1.first_name} #{@customer1.last_name}")
+    expect(page).to have_content(@customer1.address)
+    expect(page).to have_content("#{@customer1.city}, #{@customer1.state} #{@customer1.zip}")
 
-    expect(page).to_not have_content("#{@c2.first_name} #{@c2.last_name}")
+    expect(page).to have_no_content("#{@customer2.first_name} #{@customer2.last_name}")
   end
 
   it 'should display all the items on the invoice' do
-    expect(page).to have_content(@item_1.name)
-    expect(page).to have_content(@item_2.name)
+    expect(page).to have_content(@item1.name)
+    expect(page).to have_content(@item2.name)
 
-    expect(page).to have_content(@ii_1.quantity)
-    expect(page).to have_content(@ii_2.quantity)
+    expect(page).to have_content(@ii1.quantity)
+    expect(page).to have_content(@ii2.quantity)
 
-    expect(page).to have_content("$#{@ii_1.unit_price}")
-    expect(page).to have_content("$#{@ii_2.unit_price}")
+    expect(page).to have_content("$#{@ii1.unit_price}")
+    expect(page).to have_content("$#{@ii2.unit_price}")
 
-    expect(page).to have_content(@ii_1.status)
-    expect(page).to have_content(@ii_2.status)
+    expect(page).to have_content(@ii1.status)
+    expect(page).to have_content(@ii2.status)
 
-    expect(page).to_not have_content(@ii_3.quantity)
-    expect(page).to_not have_content("$#{@ii_3.unit_price}")
-    expect(page).to_not have_content(@ii_3.status)
+    expect(page).to have_no_content(@ii3.quantity)
+    expect(page).to have_no_content("$#{@ii3.unit_price}")
+    expect(page).to have_no_content(@ii3.status)
   end
 
-  it 'should display the total revenue the invoice will generate' do
-    expect(page).to have_content("Total Revenue: $#{@i1.total_revenue}")
+  it 'displays the total revenue (NOT including discounts) for an admin from a specific invoice' do
+    expect(page).to have_content("Total Revenue: $#{@invoice1.total_revenue / 100}")
 
-    expect(page).to_not have_content(@i2.total_revenue)
+    expect(page).to have_no_content(@invoice2.total_revenue)
   end
 
   it 'should have status as a select field that updates the invoices status' do
-    within("#status-update-#{@i1.id}") do
+    within("#status-update-#{@invoice1.id}") do
       select('cancelled', :from => 'invoice[status]')
       expect(page).to have_button('Update Invoice')
       click_button 'Update Invoice'
 
-      expect(current_path).to eq(admin_invoice_path(@i1))
-      expect(@i1.status).to eq('completed')
+      expect(current_path).to eq(admin_invoice_path(@invoice1))
+      expect(@invoice1.status).to eq('completed')
     end
+  end
+
+  it 'displays the total discounted revenue for an admin from a specific invoice which INCLUDES bulk discounts in the calculation' do
+
+    # And I see the total discounted revenue from this invoice which includes bulk discounts in the calculation
   end
 end
