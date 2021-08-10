944e528f91492ff35f5ddf1c968d099e3b99ea70
Caroline Tan <carolinectan3@gmail.com>
2021-08-10 01:10:30 -0400

    Add test for applying the highest discount an invoice item applies for

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -19,12 +19,14 @@ RSpec.describe InvoiceItem, type: :model do
     describe ".discount_applied" do
       it "(EXAMPLE 1) returns nil if the invoice item doesn't meet the bulk discount's quantity threshold" do
         merchant1 = Merchant.create!(name: 'Potato Stand')
+
         bulk_discount1 = merchant1.bulk_discounts.create!(percentage: 20, quantity: 10)
 
         item1 = merchant1.items.create!(name: 'Purple Potato', description: 'Very purple', unit_price: 1405)
         item2 = merchant1.items.create!(name: 'Yukon Gold Potato', description: 'Large and gold', unit_price: 2800)
 
         customer1 = Customer.create!(first_name: 'Polly', last_name: 'Pocket')
+
         invoice1 = customer1.invoices.create!(status: 2)
 
         invoiceitem1 = InvoiceItem.create!(invoice_id: invoice1.id, item_id: item1.id, quantity: 5, unit_price: 2005, status: 2)
@@ -36,12 +38,14 @@ RSpec.describe InvoiceItem, type: :model do
 
       it "(EXAMPLE 2) applies a discount only to invoice items that meet the bulk discount's quantity threshold" do
         merchant1 = Merchant.create!(name: 'Potato Stand')
+
         bulk_discount1 = merchant1.bulk_discounts.create!(percentage: 20, quantity: 10)
 
         item1 = merchant1.items.create!(name: 'Purple Potato', description: 'Very purple', unit_price: 1405)
         item2 = merchant1.items.create!(name: 'Yukon Gold Potato', description: 'Large and gold', unit_price: 2800)
 
         customer1 = Customer.create!(first_name: 'Polly', last_name: 'Pocket')
+
         invoice1 = customer1.invoices.create!(status: 2)
 
         invoiceitem1 = InvoiceItem.create!(invoice_id: invoice1.id, item_id: item1.id, quantity: 10, unit_price: 2005, status: 2)
@@ -49,6 +53,26 @@ RSpec.describe InvoiceItem, type: :model do
 
         expect(invoiceitem1.discount_applied).to eq(bulk_discount1)
         expect(invoiceitem2.discount_applied).to eq(nil)
+      end
+
+      it "(EXAMPLE 3) applies the highest discount an invoice items qualifies for" do
+        merchant1 = Merchant.create!(name: 'Potato Stand')
+
+        bulk_discount1 = merchant1.bulk_discounts.create!(percentage: 20, quantity: 10)
+        bulk_discount2 = merchant1.bulk_discounts.create!(percentage: 30, quantity: 15)
+
+        item1 = merchant1.items.create!(name: 'Purple Potato', description: 'Very purple', unit_price: 1405)
+        item2 = merchant1.items.create!(name: 'Yukon Gold Potato', description: 'Large and gold', unit_price: 2800)
+
+        customer1 = Customer.create!(first_name: 'Polly', last_name: 'Pocket')
+
+        invoice1 = customer1.invoices.create!(status: 2)
+
+        invoiceitem1 = InvoiceItem.create!(invoice_id: invoice1.id, item_id: item1.id, quantity: 12, unit_price: 2005, status: 2)
+        invoiceitem2 = InvoiceItem.create!(invoice_id: invoice1.id, item_id: item2.id, quantity: 15, unit_price: 3000, status: 2)
+
+        expect(invoiceitem1.discount_applied).to eq(bulk_discount1)
+        expect(invoiceitem2.discount_applied).to eq(bulk_discount2)
       end
     end
   end
